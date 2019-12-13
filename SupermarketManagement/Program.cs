1d8aac9525842b9268bc75f3256f1bb624cb18bf
Duong Pham Thai <duong1090@gmail.com>
2019-12-13 09:19:38 +0700

    suaw j do

--- SupermarketManagement/Program.cs
+++ SupermarketManagement/Program.cs
@@ -19,6 +19,6 @@ namespace SupermarketManagement
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startu
     }
 }
